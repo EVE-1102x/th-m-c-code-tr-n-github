29551caf05e24ffeed8ef9e726e2fd4670beae7d
dienducminh <dienducminh2201@gmail.com>
2023-02-18 21:11:01 +0700

    day là thu muc seafood chua anh seafood

---
+++ pages/seafood/crab.js
@@ -0,0 +1,15 @@
+import Link from 'next/link'
+
+export default function Crab() {
+    return(
+        <>
+        <h1>cua bể sốt me - cua Alaska sốt BBQ - Bún nem cua bể</h1>
+        <h2>calendar nghĩa là: lịch</h2>
+        <h2>
+            <Link href="/">
+                Tranng chủ
+            </Link>
+        </h2>
+        </>
+    )
+}
---
+++ pages/seafood/lobster.js
@@ -0,0 +1,14 @@
+import Link from 'next/link'
+
+export default function Lopster() {
+    return(
+        <>
+        <h1>tôm hùm lò phomai - tôm hùm sú nướng BBQ - tôm tích hấp sả</h1>
+        <h2>
+            <Link href="/">
+                Tranng chủ
+            </Link>
+        </h2>
+        </>
+    )
+}
